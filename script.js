63c03d75813b0735e6242c31b6c691a24d6744f2
KarolisZIU <karolinho3216@gmail.com>
2026-09-04 22:35:12 +0100

    calculator made functional, added functions to help that

--- script.js
+++ script.js
@@ -42,11 +42,57 @@ function operate(a, b, operator) {
   }
 }
 
-console.log(operate(10, 5, "/"));
+// console.log(operate(10, 5, "/"));
 
-//const input = document.getElementById("inputBox");
-//const oneButton = document.getElementById("one");
+const display = document.querySelector(".display");
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.getElementById("clear");
 
-//oneButton.addEventListener("click", function () {
-//input.textContent = input.textContent + "1";
-//});
+function updateNumber(event) {
+  const digit = event.target.textContent; // which number was clicked
+  if (operator == "") {
+    a = a + digit; // build up the variable
+    display.textContent = a; // show it on screen\
+  } else {
+    b = b + digit;
+    display.textContent = a + operator + b;
+  }
+}
+
+digitButtons.forEach(function (button) {
+  button.addEventListener("click", updateNumber);
+});
+
+function updateOperator(event) {
+  const op = event.target.textContent;
+  if (b !== "") {
+    calculate();
+  }
+  operator = op;
+  display.textContent = a + operator;
+}
+
+operatorButtons.forEach(function (button) {
+  button.addEventListener("click", updateOperator);
+});
+
+function calculate() {
+  const result = operate(parseInt(a), parseInt(b), operator);
+  display.textContent = result;
+  a = result;
+  b = "";
+  operator = "";
+}
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", calculate);
+
+function clearDisplay() {
+  display.textContent = "";
+  a = "";
+  b = "";
+  operator = "";
+}
+
+clearButton.addEventListener("click", clearDisplay);
